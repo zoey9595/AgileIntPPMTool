bc66775eb30396cb91652997f29fc92ba5e7bdcc
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-26 18:11:16 +1100

    Initialsed react project

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
