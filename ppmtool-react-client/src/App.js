082d8c6f039ac88369005d0a4f6dea6088832b50
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-27 15:39:38 +1100

    Imported templates

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,7 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
+
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
