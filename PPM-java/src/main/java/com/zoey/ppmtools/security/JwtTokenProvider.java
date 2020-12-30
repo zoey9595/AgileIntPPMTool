f41879f6c1292ea2ec99a3cc718f18ca04396a6e
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-31 10:34:37 +1100

    Custom JWT filter to user our tokens

--- PPM-java/src/main/java/com/zoey/ppmtools/security/JwtTokenProvider.java
+++ PPM-java/src/main/java/com/zoey/ppmtools/security/JwtTokenProvider.java
@@ -1,8 +1,7 @@
 package com.zoey.ppmtools.security;
 
 import com.zoey.ppmtools.domain.User;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -39,6 +38,27 @@ public class JwtTokenProvider {
     }
 
     // validate the token
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT Token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT Token");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
 
     // get the user id from the token
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        return Long.parseLong((String) claims.get("id"));
+    }
 }
--- PPM-java/src/main/java/com/zoey/ppmtools/security/SecurityConfig.java
+++ PPM-java/src/main/java/com/zoey/ppmtools/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.zoey.ppmtools.security.SecurityConstants.H2_URL;
 import static com.zoey.ppmtools.security.SecurityConstants.SIGN_UP_URLS;
@@ -31,6 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -71,5 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
