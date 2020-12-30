4f06b656bfe16a206fc9d49a88a5977d32ec3438
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-30 17:34:55 +1100

    Token generated

---
+++ PPM-java/src/main/java/com/zoey/ppmtools/security/JwtTokenProvider.java
@@ -0,0 +1,44 @@
+package com.zoey.ppmtools.security;
+
+import com.zoey.ppmtools.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.zoey.ppmtools.security.SecurityConstants.EXPIRATION_TIME;
+import static com.zoey.ppmtools.security.SecurityConstants.SECRET;
+
+@Component
+public class JwtTokenProvider {
+
+    // generate the token
+    public String generateToken(Authentication authentication) {
+
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullname", user.getFullName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    // validate the token
+
+    // get the user id from the token
+}
